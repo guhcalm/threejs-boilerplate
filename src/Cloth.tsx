eafa8fd810dcfa54e929040fee8c25ab37f4be86
guhcalm <guhcalm@outlook.com>
2022-08-08 20:58:55 -0400

    main

--- src/Cloth.tsx
+++ src/Cloth.tsx
@@ -1,19 +1,20 @@
 import { PlaneBufferGeometry } from "three"
 
 const getEdgesBuffer = (count: number, array: Float32Array) => {
-  const edges = {}
+  const edges = new Set<string>()
   for (let i = 0; i < count; i++) {
     const vertex = [array[i]]
     if ((i + 1) % 3 === 0) vertex.push(array[i - 2])
     else vertex.push(array[i + 1])
     vertex.sort((a, b) => a - b)
-    edges[`${vertex[0]}|${vertex[1]}`] = "edge"
+    edges.add(`${vertex[0]}|${vertex[1]}`)
   }
-  const buffer = Object.keys(edges)
+  const buffer = [...edges]
     .map(edge => edge.split("|").map(point => Number(point)))
     .flat()
   return { count: buffer.length, array: buffer, itemSize: 2 }
 }
+
 const geometry = new PlaneBufferGeometry(10, 10)
 const { count, array } = geometry.index
 const edgesBuffer = getEdgesBuffer(count, array)
